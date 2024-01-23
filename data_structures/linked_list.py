af9e1245143b25d0d3820acb2d998aba5b01aa7b
gotsulyakk <igorgotsu@gmail.com>
2024-01-23 13:49:25 +0200

    upd ll

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -3,12 +3,13 @@ class Node:
         self.value = value
         self.next = None
 
+
 class LinkedList:
     def __init__(self, value):
-        new_node = Node(value)  
-        self.head = new_node  
-        self.tail = new_node 
-        self.length = 1  
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
 
     def print_list(self):
         temp = self.head  # start at the head
@@ -17,16 +18,16 @@ class LinkedList:
             temp = temp.next  # move to the next node
 
     def append(self, value):
-        '''Append a node to the end of the list
+        """Append a node to the end of the list
 
         1. Create a new node with the value passed to the function
         2. If the linked list is empty:
             a. Set the head to be the new node
             b. Set the tail to be the new node
         3. If the linked list is not empty:
-            a. Set the next property on the tail to be that node        
+            a. Set the next property on the tail to be that node
         4. Increment the length by 1
-        '''
+        """
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
@@ -34,15 +35,15 @@ class LinkedList:
             self.length = 1
         else:
             self.tail.next = new_node
-            self.tail = None
+            self.tail = new_node
         self.length += 1
         return True
-    
+
     def pop(self):
         # if the linked list is empty
         if self.length == 0:
             return None
-        
+
         # if the linked list contains 2 or more items
         temp = self.head
         pre = self.head
@@ -61,10 +62,10 @@ class LinkedList:
             self.tail = None
 
         return temp  # return popped item
-    
+
     def prepend(self, value):
         new_node = Node(value)
-        
+
         if self.length == 0:
             self.head = new_node
             self.tail = new_node
@@ -73,13 +74,40 @@ class LinkedList:
             self.head = new_node
 
         self.length += 1
-        return True 
+        return True
 
-    # TODO: Implement the rest of methods
+    def pop_first(self):
+        if self.length == 0:
+            return None
+
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        return False
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Create a new Linked List
     ll = LinkedList(1)
     ll.print_list()
@@ -100,3 +128,21 @@ if __name__ == '__main__':
     ll.prepend(value)
     print("Linked List after prepend:")
     ll.print_list()
+
+    # Pop first
+    print("Pop first")
+    ll.pop_first()
+    ll.print_list()
+
+    # Get
+    ll.append(54)
+    ll.append(109)
+    ll.append(6)
+    index = 2
+    value = ll.get(index)
+    print(f"Got value {value.value} on index {index}")
+
+    # Set value
+    value = 666
+    ll.set_value(index, value)
+    print(f"New value of index {index} is {ll.get(index).value}")
